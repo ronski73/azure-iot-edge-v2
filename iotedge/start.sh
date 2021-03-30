721ddcb9df39a6d5386983aff78b8f6be3c4530b
ronski73 <ron_hendriks@yahoo.com>
2021-03-31 00:53:25 +0200

    first light!
    got initial deployment of edge agent etc working, not connecting to hub

--- iotedge/start.sh
+++ iotedge/start.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-# Launch docker daemon in our service
-dockerd & 2>/dev/null 
+# # remove docker.pid if needed
+# rm /var/run/docker.pid
+
+# # Launch docker daemon in our service
+ dockerd & 2>/dev/null
 
 #wait for docker to start.
 sleep 5s
+
+# # Configure connection string
+# sed -i '/device_connection_string/c\  device_connection_string : \"'${DEVICE_CONNECTION_STRING}'\"' /etc/iotedge/config.yaml
+
+# # Configure docker socket
+# sudo touch /var/lib/iotedge/mgmt.sock
+# sudo touch /var/lib/iotedge/workload.sock
+
+# sudo chown iotedge:iotedge /var/lib/iotedge/mgmt.sock
+# sudo chown iotedge:iotedge /var/lib/iotedge/workload.sock
+
+# sudo chmod 660 /var/lib/iotedge/mgmt.sock
+# sudo chmod 666 /var/lib/iotedge/workload.sock
+
+
 #Launch iotedge
-/usr/bin/iotedged -c config.yaml
+/usr/bin/iotedged -c /etc/iotedge/config.yaml
 
 while :
 do
